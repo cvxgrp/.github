2bd3afb596556d68561761982e9ab2ba25d0c85e
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2024-06-09 12:52:49 +0400

    test for Python code

--- tests/python/test_trivial.py
+++ tests/python/test_trivial.py
@@ -1,10 +1,7 @@
-from cvx.dog import Dog
-
-
 def test_trivial():
     assert True
 
 
-def test_dog():
-    dog = Dog(name="Peter Maffay")
-    assert dog.name == "Peter Maffay"
+# def test_dog():
+#    dog = Dog(name="Peter Maffay")
+#    assert dog.name == "Peter Maffay"
